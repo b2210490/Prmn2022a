b34862fc41daf852e9bcfc84168f7d2832aa9403
b2210490 <b2210490@photon.chitose.ac.jp>
2022-11-01 17:31:01 +0900

    kadai12

---
+++ Prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+        int x = 2210490;
+        System.out.println("B"+ x + " Atsushi Otsuka " );
+    }
+}
