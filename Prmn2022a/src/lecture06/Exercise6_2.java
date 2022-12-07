d326aa097e287c03d2ffa5c042bab81b351e6063
b2210490 <b2210490@photon.chitose.ac.jp>
2022-12-07 10:56:31 +0900

    kadai6-2,6-3完成

--- Prmn2022a/src/lecture06/Exercise6_2.java
+++ Prmn2022a/src/lecture06/Exercise6_2.java
@@ -1,17 +1,13 @@
 package lecture06;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise6_2 {
     public static void main(String[] args) {
         Random random = new Random();
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<5; i++) {
-        int x = random.nextInt(6) + 1;
-        list.add(x);
+            list.add(random.nextInt(6) +1);
         }
 
         System.out.println("さいころを5つ振りました。");
@@ -20,11 +16,11 @@ public class Exercise6_2 {
             Scanner input = new Scanner(System.in);
             System.out.println("何番目のサイコロの値を確認しますか？");
             int x = input.nextInt();
-            System.out.println();
+            System.out.println(list.get(x));
         } catch (IndexOutOfBoundsException e){
             System.out.println("ArrayListの範囲外のアクセスを確認しました。");
             System.out.println("プログラムを終了します。");
-        } catch (IllegalArgumentException e) {
+        } catch (InputMismatchException e) {
             System.out.println("整数以外の値が入力されました。");
             System.out.println("プログラムを終了します。");
         }
